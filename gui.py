515d342387d797c0ea8b02fb46e0592a9a21bcbf
Yuvraj <raghavyuvraj596@gmail.com>
2025-06-18 12:05:44 +0530

    handle error caused by empty string in add and edit

--- gui.py
+++ gui.py
@@ -3,13 +3,17 @@ import functions
 
 import FreeSimpleGUI as sg
 import  time
+import os
 
+if not os.path.exists("todos.txt"):
+    with open("todos.txt",'w') as file:
+        pass
 sg.theme("Black")
 
 clock = sg.Text('',key = 'clock')
 label = sg.Text("Type in a To-Do")
 input_box = sg.InputText(tooltip="Enter todo",key = "todo")
-add_button =sg.Button("Add",size=10)
+add_button =sg.Button("Add",size=10,)
 
 list_box = sg.Listbox(values=functions.get_todos(),key='todos',
                       enable_events=True,size=[45,10])
@@ -32,23 +36,26 @@ while True:
     window['clock'].update(value=time.strftime("%b %d, %Y %H:%M:%S"))
     match event:
         case "Add":
-
-            #try:
-                todos = functions.get_todos()
-                new_todo =values['todo'] + "\n"
-                todos.append(new_todo)
-                functions.write_todos(todos)
-                window['todos'].update(values = todos)
+                new_todo = values['todo'].strip()
+                if new_todo=="":
+                    sg.popup("Please enter a to-do item")
+                else:
+                    todos = functions.get_todos()
+                    todos.append(new_todo +"\n")
+                    functions.write_todos(todos)
+                    window['todos'].update(values = todos)
         case "Edit":
             try:
               todo_to_edit = values['todos'][0]
               new_todo = values['todo']
-
-              todos = functions.get_todos()
-              index = todos.index(todo_to_edit)
-              todos[index] = new_todo
-              functions.write_todos(todos)
-              window['todos'].update(values=todos)
+              if new_todo=="":
+                  sg.popup("Please Enter a new value to Edit.")
+              else:
+                  todos = functions.get_todos()
+                  index = todos.index(todo_to_edit)
+                  todos[index] = new_todo
+                  functions.write_todos(todos)
+                  window['todos'].update(values=todos)
             except IndexError:
                 sg.popup("Please select an item first",font=("Helvetica",20))
 
